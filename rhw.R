306a2d794019ff729843751101a71e699795ddce
mooret5 <tiaranmoore@gmail.com>
2015-09-23 01:46:24 -0400

    hw part 1

---
+++ rhw.R
@@ -0,0 +1,19 @@
+#Alfaro assugnment
+
+for (ii in 1:9) { print ("*")}
+for (ii in 1:10) { print ("*&")}
+dogs <- 10;
+for (i in 1:5){
+  dogs <- dogs + 1; 
+}
+###
+meatloaf <- 0; 
+for (i in 5:9){
+  meatloaf <- meatloaf - i + 1;
+  cat(meatloaf) 
+}
+###
+bubbles <- 12;
+for (i in -1:-4){
+  bubbles <- i;
+}
