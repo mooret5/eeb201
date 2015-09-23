06d8892bd1e93915cd101478443ba25f47a501d4
mooret5 <tiaranmoore@gmail.com>
2015-09-23 17:43:57 -0400

    working on hw1

--- rhw.R
+++ rhw.R
@@ -1,7 +1,10 @@
 #Alfaro assugnment
 
+#1
 for (ii in 1:9) { print ("*")}
+#2
 for (ii in 1:10) { print ("*&")}
+#3
 dogs <- 10;
 for (i in 1:5){
   dogs <- dogs + 1; 
@@ -17,3 +20,20 @@ bubbles <- 12;
 for (i in -1:-4){
   bubbles <- i;
 }
+#4
+###you can use the if statement with the modulus operator to conditionally perform operations
+years <- c( 2015, 2016, 2018, 2020, 2021)
+for(ii in 1:length(years)){
+  if(years[ii] %% 4 == 0){
+    cat(years[ii], 'Hooray, presidential elections!', sep = '\t', fill = T)
+  }
+  
+}
+#5
+bankAccounts <- c(10, 9.2, 5.6, 3.7, 8.8, 0.5);
+
+#Now look at the error message the following lines of code produce. Can you think of a way to modify this loop so that the loop will compund the interest?
+
+interestRate <- 0.0125;
+for (i in 1:length(bankAccounts)) {
+  compounded[i] <- interestRate*bankAccounts[i] + bankAccounts[i]; }
