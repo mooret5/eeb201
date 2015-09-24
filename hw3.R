4ebd73c35bd18c8ef00841879363c093a14f304f
mooret5 <tiaranmoore@gmail.com>
2015-09-24 17:17:01 -0400

    strugglin

--- hw3.R
+++ hw3.R
@@ -1,4 +1,6 @@
-compute_chisquare=(“hapmap_CEU_r23a_chr2_ld.txt"){
+snpsDataFrame=read.table('hapmap_CEU_r23a_chr2_ld-1.txt',header=TRUE)
+
+compute_chisquare=function(x){
   freq=sum(x,na.rm=TRUE)/(2.0*sum(!is.na(x)))
   cnt0=sum(x==0,na.rm=TRUE)
   cnt1=sum(x==1,na.rm=TRUE)
@@ -11,4 +13,44 @@ compute_chisquare=(“hapmap_CEU_r23a_chr2_ld.txt"){
   return(chisq)
 }
 
-pvals <- obscnts  
+compute_chisquare_2=function(x){
+  freq=sum(x,na.rm=TRUE)/(2.0*sum(!is.na(x)))
+  cnt0=sum(x==0,na.rm=TRUE)
+  cnt1=sum(x==1,na.rm=TRUE)
+  cnt2=sum(x==2,na.rm=TRUE)
+  obscnts=c(cnt0,cnt1,cnt2)
+  #print(obscnts)
+  n=sum(obscnts)
+  #here we use the built-in function for the chi-sq distribution:
+  exp_pvals=c((1-freq)^2,2*freq*(1-freq),freq^2) #note, here we don't multiply by n
+  chisq<-chisq.test(obscnts,p=exp_probs, correct = FALSE)$statistic
+  return(chisq)
+}
+
+
+chisqs=apply(snps,1,compute_chisquare)
+chisqs2=apply(snps,1,compute_chisquare_2)
+
+pvals=pchisq(chisqs,1,lower.tail=FALSE)
+
+signifthres<-0.05
+sum(pvals<signifthres) 
+mean(pvals<signifthres) 
+
+signifthres<-0.01
+sum(pvals<signifthres) 
+mean(pvals<signifthres) 
+
+signifthres<-0.001
+sum(pvals<signifthres) 
+mean(pvals<signifthres) 
+
+length(pvals)
+num_pval <- length(pvals)
+sort_pvals <- sort(pvals)
+log_sort_pvals <- -log10(sort_pvals)
+log_exp_pvals <- -log10(pvals)
+plot(log_exp_pvals, log_sort_pvals)
+
+
+
