3638eaf48356a6406dda93880c15d4411399b326
mooret5 <tiaranmoore@gmail.com>
2015-09-24 18:42:56 -0400

    finishing last part of hw!

--- hw3.R
+++ hw3.R
@@ -1,3 +1,5 @@
+#part 1
+
 snpsDataFrame=read.table('hapmap_CEU_r23a_chr2_ld-1.txt',header=TRUE)
 
 compute_chisquare=function(x){
@@ -52,5 +54,33 @@ log_sort_pvals <- -log10(sort_pvals)
 log_exp_pvals <- -log10(pvals)
 plot(log_exp_pvals, log_sort_pvals)
 
+#part 2
 
+zz=read.table("pheno.sim.2014-1.txt",header=TRUE)
+
+plot(zz,col=2,lwd=4,xlab="Value",xlim=c(-15,15),main="Blood Glucose")
+abline(v=quantile(0.25),lty=2,lwd=3,col=4) #puts a vertical line onto the plot
+abline(v=quantile(0.75),lty=2,lwd=3,col=5)
+
+
+
+case_genotypes=snps["rs7584086_T",])
+control_genotypes=factor(snps["rs7584086_T",])
+m=lm(case_genotypes)
+summary(m)
+
+m1=2m1(case_genotypes)
+summary(m1)
+
+m2=0m2(case_genotypes)
+summary(m2)
+
+t=lt(control_genotypes)
+summary(t)
+
+t1=2t1(control_genotypes)
+summary(t1)
+
+t2=0t2(control_genotypes)
+summary(t2)
 
