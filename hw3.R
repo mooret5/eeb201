22e2b007fb0387e643debcb04520f552fd2a4cfb
mooret5 <tiaranmoore@gmail.com>
2015-09-24 16:26:01 -0400

    trying to figure out chi squares!!

---
+++ hw3.R
@@ -0,0 +1,14 @@
+compute_chisquare=(“hapmap_CEU_r23a_chr2_ld.txt"){
+  freq=sum(x,na.rm=TRUE)/(2.0*sum(!is.na(x)))
+  cnt0=sum(x==0,na.rm=TRUE)
+  cnt1=sum(x==1,na.rm=TRUE)
+  cnt2=sum(x==2,na.rm=TRUE)
+  obscnts=c(cnt0,cnt1,cnt2)
+  #print(obscnts)
+  n=sum(obscnts)
+  expcnts=c((1-freq)^2,2*freq*(1-freq),freq^2)*n
+  chisq=sum((obscnts-expcnts)^2/expcnts)
+  return(chisq)
+}
+
+pvals <- obscnts  
